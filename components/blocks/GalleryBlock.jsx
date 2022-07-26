7027502c28498e0aaa78fed87a648ddacf0682e6
sha5b <ned.tabulov@gmail.com>
2022-07-26 19:39:43 +0200

    Reversed conditional Rendering

--- components/blocks/GalleryBlock.jsx
+++ components/blocks/GalleryBlock.jsx
@@ -3,22 +3,6 @@ import Image from "next/image";
 export const GalleryBlock = ({ block, i }) => {
   return (
     <div key={block.id + i}>
-      {block.gallery
-        ? block.gallery?.map((item, i) => {
-            return (
-              <div>
-                <Image
-                  width="500"
-                  height="100%"
-                  quality="100"
-                  pos="relative"
-                  src={item.image}
-                  alt={item.alt}
-                />
-              </div>
-            );
-          })
-        : null}
     </div>
   );
 };
--- components/blocks/LogoBLock.jsx
+++ components/blocks/LogoBLock.jsx
@@ -4,23 +4,6 @@ export const LogoBlock = ({ block, i }) => {
   return (
     <div key={block.id + i}>
       <div>{block.headline}</div>
-      {block
-        ? block.logos?.map((item, i) => {
-            return (
-              <div>
-                <Image
-                  width="125"
-                  height="100%"
-                  quality="100"
-                  pos="relative"
-                  objectFit="fill"
-                  src={item.logo}
-                  alt={item.logo}
-                />
-              </div>
-            );
-          })
-        : null}
     </div>
   );
 };

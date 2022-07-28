2ff192e75bff17429b337dc90a42285df25e6444
sha5b <ned.tabulov@gmail.com>
2022-07-28 21:27:24 +0200

    Push of the Day
    Just a safety  repercussion.

--- components/blocks/HeroBlock.jsx
+++ components/blocks/HeroBlock.jsx
@@ -10,26 +10,28 @@ const Img = chakra(Image, {
 
 export const HeroBlock = ({ block, id, i }) => {
   return (
-    <Box pt={"2rem"}>
+    <Box pt={"2rem"} key={id + i}>
       <Grid
         templateColumns={"1fr 1fr 1fr 1fr"}
         templateRows={"0.2fr 1fr 0.2fr"}
         gap={5}
       >
         <GridItem rowSpan={2} colSpan={2}>
-          <Box pt={"5rem"}>
+          <Box pos="relative">
             <Heading fontSize={"6xl"}>{block.title}</Heading>
             <Text fontSize={"3xl"}>{block.subtitle}</Text>
           </Box>
         </GridItem>
         {block.image && (
-          <GridItem rowSpan={3} colSpan={2} >
+          <GridItem rowSpan={3} colSpan={2}>
             <Link href="posts/">
-              <Box p={"1.5rem"} bg={"orangebiz.100"}>
-                <Box pos="relative" width="720" height="500">
+              <Box p={"1.5rem"} bg={"orangebiz.100"} boxShadow={"md"}>
+                <Box pos="relative" display={"block"}>
                   <Img
                     quality="100"
-                    layout="fill"
+                    width="100%"
+                    height="75%"
+                    layout="responsive"
                     objectFit="cover"
                     rounded={"1.5rem"}
                     src={block.image}
--- components/blocks/QuoteBlock.jsx
+++ components/blocks/QuoteBlock.jsx
@@ -1,15 +1,18 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Heading, Text } from "@chakra-ui/react";
 
 export const QuoteBlock = ({ block, id, i }) => {
   return (
-    <Box key={id + i}>
-      <Heading fontSize="5xl">
-        {block.quote}
-      </Heading>
-      {block.author && 
-      <Text fontSize="3xl">
-        - "{block.author}"
-      </Text>}
+    <Box pt={"2rem"} key={id + i}>
+      <Grid
+        templateColumns={"1fr 1fr 1fr 1fr"}
+        templateRows={"0.2fr 1fr 0.2fr"}
+        gap={5}
+      >
+        <GridItem rowSpan={2} colSpan={4}>
+          <Heading fontSize="5xl">{block.quote}</Heading>
+          {block.author && <Text fontSize="3xl">- "{block.author}"</Text>}
+        </GridItem>
+      </Grid>
     </Box>
   );
 };
